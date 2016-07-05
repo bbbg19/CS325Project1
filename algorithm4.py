893a333592b6c62f7af70cb06599d00e94f3cf9b
James Grunewald <grunewaj@oregonstate.edu>
2016-07-05 15:50:34 -0700

    Add files via upload

---
+++ algorithm4.py
@@ -0,0 +1,20 @@
+var = []
+
+def Linear(A)
+    n = length(A)
+    max_sum = -1
+    ending_here_sum = -1
+    for j in range(0, n-1)
+        ending_here_high = j
+        if ending_here_sum > 0
+            ending_here_sum = ending_here_sum + A[j]
+        else ending_here_low = j
+            ending_here_sum = A[j]
+        if ending_here_sum > max_sum
+            max_sum =ending_here_sum
+            low = ending_here_sum
+            high = ending_here_high
+    return max_sum
+            
+    
+    
