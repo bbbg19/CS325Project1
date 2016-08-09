a04e52b8e27fdd641ee9bd69d6b020736200129c
James Grunewald <grunewaj@oregonstate.edu>
2016-08-09 09:11:49 -0700

    Updated to fix the errors

--- project4_greedy.py
+++ project4_greedy.py
@@ -3,6 +3,14 @@
 import math, re, sys
 import time
 
+
+#Altered to calculate the distance from the origin    
+def distanceOrigin(x, y):
+	calcDistance = round(math.sqrt(math.pow(x,2) + math.pow(y,2)))
+	return calcDistance
+
+
+
 #Read in input files and return array 
 def read_input_vals(in_file):
 	points = []
@@ -11,7 +19,6 @@ def read_input_vals(in_file):
 	
 	while len(line) > 1:
 		line_parse = re.findall(r'[^,;\s]+', line)
-		print line
 		index = int(line_parse[0])
 		x = int(line_parse[1])
 		y = int(line_parse[2])
@@ -22,21 +29,15 @@ def read_input_vals(in_file):
 	return points 
 
 
-	
+
+
 def printResults (outfile, resultsData, distanceCovered):
 	print "Results can be found in " + outfile
 	outFile = open(outfile,'w')
 	outFile.write(str(int(distanceCovered)) + '\n')
 	for index, points in enumerate(resultsData):
-		outFile.write(str(points) + '\n' )	
+		outFile.write(str(points['index']) + '\n' )	
 	return
-	
-
-    
-#Altered to calculate the distance from the origin    
-def distanceOrigin(x, y):
-	calcDistance = round(math.sqrt(math.pow(x,2) + math.pow(y,2)))
-	return calcDistance
 
 
     
@@ -47,10 +48,9 @@ def distance(point1, point2):
 	
 def main(inputFile, outputFile):
 	#Read in input Files
-	
 	input_point_labels = read_input_vals(inputFile)
 	
-	print (distance(input_point_labels[1],input_point_labels[2]))
+	
 	#Final Algorithm
 	start = time.time();
 	distanceCovered, path = tspRun(input_point_labels)
@@ -58,45 +58,51 @@ def main(inputFile, outputFile):
 	
 	#Print out results 
 	printResults(outputFile, path, distanceCovered)
-	
+
+    
+def distGetter(point):
+    return point['distance']
+    
+    
 	
 def tspRun(inputValues):
     path =[] 
     distanceCovered = 0 
-    sortedPoints = sorted(inputValues, key=attrgetter('distance'))
+    sortedPoints = sorted(inputValues, key=distGetter)
     path.append(sortedPoints.pop()) 
     path.insert(0, sortedPoints.pop()) 
     distanceCovered += distance(path[0], path[1]) 
     path.append(sortedPoints.pop()) 
     distanceCovered += distance(path[-1], path[-2]) 
+    print distanceCovered
     endCheck = 0 
     
 	#While there are still points in the sortedPoints array continue
     while len(sortedPoints) > 0: 
         if endCheck == 0:
             one = distance(path[0], sortedPoints[-1]) 
-            two = distance(path[0], sortedPoints[-2]) 
+            two = distance(path[0], sortedPoints[len(sortedPoints) - 2]) 
             
             #inserts whichever point is closest and removes it from sortedPoints
             if one < two:
                 path.insert(0, sortedPoints.pop())
                 distanceCovered += one
             else:
-                path.insert(0, sortedPoints[-2])
-                del sortedPoints[-2]
+                path.insert(0, sortedPoints[len(sortedPoints) - 2])
+                del sortedPoints[len(sortedPoints) - 2]
                 distanceCovered += two
                 
         if endCheck == 1:
             one = distance(path[-1], sortedPoints[-1]) #gets the distance to the next furthest point
-            two = distance(path[-1], sortedPoints[-2]) #gets the distance to the second furthest point
+            two = distance(path[-1], sortedPoints[len(sortedPoints) - 2]) #gets the distance to the second furthest point
             
             #inserts whichever point is closest and removes it from sortedPoints
             if one < two:
                 path.append(sortedPoints.pop())
                 distanceCovered += one
             else:
-                path.append(sortedPoints[-2])
-                del sortedPoints[-2]
+                path.append(sortedPoints[len(sortedPoints) - 2])
+                del sortedPoints[len(sortedPoints) - 2]
                 distanceCovered += two
         if endCheck == 0:
             endCheck = 1
